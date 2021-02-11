75a2c088a8458be5c0b080eece8c24f12d72559c
donnicholaus@gmail.com <gaetanamD13>
2021-02-11 16:05:15 +0300

    Active pages in register staff and view staff changed .. uncesesary notificatiopns removed

--- admin/register_staff.php
+++ admin/register_staff.php
@@ -49,46 +49,7 @@
               <button class="app-search__button"><i class="fa fa-search"></i></button>
             </li>
             <!--Notification Menu-->
-            <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="fa fa-bell-o fa-lg"></i></a>
-              <ul class="app-notification dropdown-menu dropdown-menu-right">
-                <li class="app-notification__title">You have 4 new notifications.</li>
-                <div class="app-notification__content">
-                  <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span>
-                      <div>
-                        <p class="app-notification__message">Lisa sent you a mail</p>
-                        <p class="app-notification__meta">2 min ago</p>
-                      </div></a></li>
-                  <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-danger"></i><i class="fa fa-hdd-o fa-stack-1x fa-inverse"></i></span></span>
-                      <div>
-                        <p class="app-notification__message">Mail server not working</p>
-                        <p class="app-notification__meta">5 min ago</p>
-                      </div></a></li>
-                  <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-success"></i><i class="fa fa-money fa-stack-1x fa-inverse"></i></span></span>
-                      <div>
-                        <p class="app-notification__message">Transaction complete</p>
-                        <p class="app-notification__meta">2 days ago</p>
-                      </div></a></li>
-                  <div class="app-notification__content">
-                    <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-primary"></i><i class="fa fa-envelope fa-stack-1x fa-inverse"></i></span></span>
-                        <div>
-                          <p class="app-notification__message">Lisa sent you a mail</p>
-                          <p class="app-notification__meta">2 min ago</p>
-                        </div></a></li>
-                    <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-danger"></i><i class="fa fa-hdd-o fa-stack-1x fa-inverse"></i></span></span>
-                        <div>
-                          <p class="app-notification__message">Mail server not working</p>
-                          <p class="app-notification__meta">5 min ago</p>
-                        </div></a></li>
-                    <li><a class="app-notification__item" href="javascript:;"><span class="app-notification__icon"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x text-success"></i><i class="fa fa-money fa-stack-1x fa-inverse"></i></span></span>
-                        <div>
-                          <p class="app-notification__message">Transaction complete</p>
-                          <p class="app-notification__meta">2 days ago</p>
-                        </div></a></li>
-                  </div>
-                </div>
-                <li class="app-notification__footer"><a href="#">See all notifications.</a></li>
-              </ul>
-            </li>
+            
             <!-- User Menu-->
             <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
               <ul class="dropdown-menu settings-menu dropdown-menu-right">
@@ -108,7 +69,7 @@
             </div>
           </div>
           <ul class="app-menu">
-            <li><a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label"> Register New Staff</span></a></li>
+            <li><a class="app-menu__item active" href="#"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label"> Register New Staff</span></a></li>
             <li><a class="app-menu__item" href="view_staff.php"><i class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label"> View Staff</span></a></li>
 
           </ul>
