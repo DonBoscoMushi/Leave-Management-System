6c4cd87599a0410b0d13fa3c057c38f415a892d8
donnicholaus@gmail.com <gaetanamD13>
2021-02-11 15:21:15 +0300

    change layout of current active page

--- staff/index.php
+++ staff/index.php
@@ -56,9 +56,9 @@
         </div>
       </div>
       <ul class="app-menu">
-        <li><a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Profile</span></a></li>
+        <li><a class="app-menu__item active" href="#"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Profile</span></a></li>
         
-        <li><a class="app-menu__item active" href="docs.html"><i class="app-menu__icon fa fa-file-code-o"></i><span class="app-menu__label">Docs</span></a></li>
+        <li><a class="app-menu__item " href="docs.html"><i class="app-menu__icon fa fa-file-code-o"></i><span class="app-menu__label">Docs</span></a></li>
       </ul>
     </aside>
     <main class="app-content">
