ee7189d3c62fe3b23764314d5284fdd89f349be9
donnicholaus@gmail.com <gaetanamD13>
2021-02-13 01:31:58 +0300

    replaced cancell button in diagol from footer to header

--- admin/edit_staff.php
+++ admin/edit_staff.php
@@ -39,6 +39,67 @@ include_once 'view_staff.php';
   </div>
   
   
+
+                <div class='tile'>
+                  <h3 class='tile-title' style='text-align: center;'><i class='fa fa-edit'></i> Register New Staff</h3>
+                  <div class='tile-body'>
+                    <form class='form-horizontal' method='post' action='register.php'>
+                      <div class='form-group row'>
+                        <label class='control-label col-md-3'>First Name</label>
+                        <div class='col-md-8'>
+                          <input class='form-control' type='text' name='firstname' required='required' placeholder='Enter first name'>
+                        </div>
+                      </div>
+                      <div class='form-group row'>
+                        <label class='control-label col-md-3'>Last Name</label>
+                        <div class='col-md-8'>
+                          <input class='form-control' type='text' name='lastname' required='required' name='lastname' placeholder='Enter last name'>
+                        </div>
+                      </div>
+                      <div class='form-group row'>
+                        <label class='control-label col-md-3'>Email</label>
+                        <div class='col-md-8'>
+                          <input class='form-control col-md-8' type='email' required='required' name='email' placeholder='Enter email address'>
+                        </div>
+                      </div>
+                      <div class='form-group row'>
+                        <label class='control-label col-md-3'>Role</label>
+                        <div class='col-md-8'>
+                          <input class='form-control col-md-8' type='text' required='required' name='role' placeholder='Enter Role'>
+                        </div>
+                      </div>
+
+                      <div class='form-group row'>
+                        <label class='control-label col-md-3'>Gender</label>
+                        <div class='col-md-9'>
+                          <div class='form-check'>
+                            <label class='form-check-label'>
+                              <input class='form-check-input' type='radio' value='male' name='gender'>Male
+                            </label>
+                          </div>
+                          <div class='form-check'>
+                            <label class='form-check-label'>
+                              <input class='form-check-input' type='radio' value='female' name='gender'>Female
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class='tile-footer'>
+                    <div class='row'>
+                      <div class='col-md-8 col-md-offset-3'>
+                        <button class='btn btn-primary' type='submit' value='Register'><i class='fa fa-fw fa-lg fa-check-circle' ></i>Register</button>&nbsp;&nbsp;&nbsp;<a class='btn btn-secondary' href='#'><i action='reset'class='fa fa-fw fa-lg fa-times-circle'></i>Cancel</a>
+                      </div>
+                    </div>
+                  </div>
+                    </form>
+                  </div>
+                </div>
+            </div>
+
+
+
+
+
   </body>
 
 </html>
