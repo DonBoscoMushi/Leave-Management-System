3e54c03baab9fb219054b0b59f1cf7ce2b2d0bc4
Gracejoseph-0303 <GraceJoseph0303@icloud.com>
2021-02-16 11:35:50 +0300

    Apply leave

--- staff/apply_leave.php
+++ staff/apply_leave.php
@@ -122,45 +122,121 @@
 
 
                   <div class="form-group">
-                    <label for="exampleInputEmail1">Leave Type</label>
-                    <input class="form-control" id="exampleInputName" type="name" aria-describedby="grace " placeholder="Enter name"><small class="form-text text-muted" id="name"></small>
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleInputPassword1">Proposed date of depature</label>
-                    <input class="form-control" id="exampleInputPlace" type="name" placeholder="name">
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleSelect1">Gross numbder of days accumulated up to the end of las academic year</label>
-                    <input class="form-control" id="exampleDepartment" type="department name placeholder="department name>
-                      
-                      
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleSelect2">Number of Leave days due for this academic year</label>
-                    <input class="form-control" id="exampleFamily" multiple="members">
-                      
-                    
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleSelect2">My leave will due to be expired on </label>
-                    <input class="form-control" id="exampleFamily" multiple="members">
-                      
-                    
-                  </div>
-                  <div class="form-group">
-                    <label for="exampleSelect2">Signature</label>
-                    <input class="form-control" id="exampleFamily" multiple="members">
-                      
-                    
-                  </div><div class="form-group">
-                    <label for="exampleSelect2">Date</label>
-                    <input class="form-control" id="exampleFamily" multiple="members">
-                      
-                    
-                  </div>
+                  <label for="leave_type" ><span>Leave Type <span class="required"></span></span>
+                  <select name="leave_type" id="leave_type">
+		 	<option value="Casual Leave">Casual Leave</option><option value="New">New</option><option value="Newest">Newest</option><option value="Sick Leave">Sick Leave</option><option value="Weekend Leave">Weekend Leave</option>       
 
+  </select>
+                    
+                    <select name="leave_type" id="leave_type">
+		 	<?PHP
+           		while ($row = mysqli_fetch_array($result))
+            	{
+                	$leave_type = $row['leave_type'];
+                	echo "<option value=\"".$leave_type."\">".$leave_type."</option>";
+            	}
+          	?>
+          	</select>
+                    </label>
+
+                    
                  
+    
+    <div class="multiple_days_leave" >     
+        <label for="start_date" ><span>Start Date <span class="required"></span></span>
+        	<input type="date" name="start_date" id="start_date" onchange="total_days()" />
+        </label>
+        
+        <label for="end_date" ><span>End Date <span class="required"></span></span>
+        	<input type="date" name="end_date" id="end_date" onchange="total_days()" />
+        </label>
+   	</div>
+    
+    <div class="leave_requested_days" >     
+        <label for="days_requested" ><span>Days Requested </span>
+        	<input type="text" name="days_requested" id="days_requested" readonly="true" placeholder="No. of Days"/>
+        </label>
+   	</div>
+                  
+
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-image: url(../images/bg.gif);
+}
+</style>
+<link href="../style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="../jquery.js"></script>
+<script>
+	function total_days()
+	{
+		var start_date = document.getElementById("start_date");
+		var end_date = document.getElementById("end_date");
+		var start_day = new Date(start_date.value);
+		var end_day = new Date(end_date.value);
+		var milliseconds_per_day = 1000 * 60 * 60 * 24;
+	
+		var millis_between = end_day.getTime() - start_day.getTime();
+		var days = millis_between / milliseconds_per_day;
+	
+		// Round down.
+		//alert( Math.floor(days));
+		
+		var total_days = (Math.floor(days)) + 1;
+		var combined = total_days;
+		//alert(combined);
+		//document.getElementById("date").value = (Math.floor(days)) ;
+		var days_requested = document.getElementById('days_requested');
+		days_requested.value = combined;
+	}
+	
+	$(document).ready(function(){
+        $('input[type="radio"]').click(function(){
+            if($(this).attr("value")=="one_day"){
+                $(".multiple_days_leave").hide();
+                $(".one_day_leave").show();
+				$(".leave_type").show();
+				$(".leave_requested_days").hide();
+				$(".button").show();
+            }
+            if($(this).attr("value")=="multiple_days"){
+                $(".one_day_leave").hide();
+                $(".multiple_days_leave").show();
+				$(".leave_type").show();
+				$(".leave_requested_days").show();
+				$(".button").show();
+            }
+        });
+    });
+	
+	$(document).ready(function(){
+    if(!$("input[type=radio][name='leave_duration']:checked").val())
+	{
+		 $(".one_day_leave").hide();
+         $(".multiple_days_leave").hide();
+		 $(".leave_type").hide();
+		 $(".leave_requested_days").hide();
+		 $(".button").hide();
+	}
+});
+</script>
+</head>
+
+<body>
+		 
+
+    
+    
+    
+    </form>
+  </div>
+ 
+</body>
+</html>
+            </div>
                  
                  
                  
